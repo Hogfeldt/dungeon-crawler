22a557e62598983443709597f6783473fad9a7c1
Downz <anderesmhandsome@gmail.com>
2019-11-02 13:21:14 +0100

    .net-core REST API Init fix

--- ServerApp/Controllers/MyController.cs
+++ ServerApp/Controllers/MyController.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
-namespace TestRestAPI.Controllers
+namespace ServerApp.Controllers
 {
     [Route("")]
     [ApiController]
@@ -31,26 +26,26 @@ namespace TestRestAPI.Controllers
             };
         }
 
-        // GET: /<number>
+        // GET: api/My/5
         [HttpGet("{id}", Name = "Get")]
         public string Get(int id)
         {
             return "url value: " + id;
         }
 
-        // POST: 
+        // POST: api/My
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT: <number>
+        // PUT: api/My/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE: <number>
+        // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
