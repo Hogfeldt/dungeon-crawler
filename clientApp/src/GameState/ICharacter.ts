82ccf596c07a5a9b9b25a85c919601e8b2a9a3a0
sagmo <sivert.sag@gmail.com>
2019-11-07 16:13:56 +0100

    oh my gud

--- clientApp/src/GameState/ICharacter.ts
+++ clientApp/src/GameState/ICharacter.ts
@@ -1,5 +1,11 @@
-
 export interface Icharacter {
-    
+    Name: string;
+    xPos: number;
+    yPos: number;
+    Health: number;
+
+    inventoryList(): any;
+    item(): 
+    printChar(): any;
 }
 
