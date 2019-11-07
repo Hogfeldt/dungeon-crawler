5ac29306332de2589bbb17302a5e8366413a1daa
Glenn Laursen <201703930@post.au.dk>
2019-11-07 15:01:14 +0100

    Better structure in test file

--- clientApp/tests/Monster.test.ts
+++ clientApp/tests/Monster.test.ts
@@ -1,12 +1,12 @@
 import { NPCMonster } from '../src/components/NPCMonster';
 
-describe('Monster', function() {
-  it('New Monster', function() {
-	  const Monster: NPCMonster = new NPCMonster(100, 50, 'BigDGlenn', 'Enemy', 0, 1);
-      expect(Monster.Name).toBe('BigDGlenn');
-  });
-  it('Monster Health', function() {
-	  const Monster: NPCMonster = new NPCMonster(100, 50, 'BigDGlenn', 'Enemy', 0, 1);
-      expect(Monster.Health).toBe(100);
-  });
- });
+describe('Monster', () => {
+    it('New Monster', () => {
+        const monster = new NPCMonster(100, 50, 'BigDGlenn', 'Enemy', 0, 1);
+        expect(monster.Name).toBe('BigDGlenn');
+    });
+    it('Monster Health', () => {
+        const monster = new NPCMonster(100, 50, 'BigDGlenn', 'Enemy', 0, 1);
+        expect(monster.Health).toBe(100);
+    });
+});
