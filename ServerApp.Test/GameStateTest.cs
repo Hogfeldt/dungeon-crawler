25b4c73b3f8a92190ccf7fb26acf79d8cb2dc5a5
srensommer <srensommer@gmail.com>
2019-11-07 16:57:35 +0100

    Tests virker nau

--- ServerApp.Test/GameStateTest.cs
+++ ServerApp.Test/GameStateTest.cs
@@ -1,9 +1,15 @@
 ﻿using System;
-
+using NUnit.Framework;
 namespace ServerApp.UnitTest
 {
     public class GameStateTest
     {
+        //[TestFixture]
 
+        [SetUp]
+        public void GameStateCreationTest()
+        {
+
+        }
     }
 }
