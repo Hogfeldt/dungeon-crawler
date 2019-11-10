53f2019934d550876db2c52ff4f587f5522abd61
EliasRosendahl <jensen.eliasrosendahl@gmail.com>
2019-11-10 17:36:15 +0100

    now works with mock backend

--- clientApp/src/ChangeHandler/ChangeHandler.ts
+++ clientApp/src/ChangeHandler/ChangeHandler.ts
@@ -9,28 +9,29 @@ import { ILayer } from "../GameState/ILayer"
 import { Layer } from "../GameState/Layer"
 
 
-export class ChangeHandler {
+export default class ChangeHandler {
     api: IApi;
     constructor(api:IApi) {
         this.api = api;
     }
 
     public getState() {
-        this.api.gameState().then((r) => {
+        return this.api.gameState().then(r => {
             var npcs: NPC[] = [];
             npcs[0] = new NPC(1, 2);
             npcs[1] = new NPC(3, 4);
 
             var layer: ILayer = new Layer();
 
-            var name:string = r.data["player"].name;
-            var xPosition:number = r.data["player"].yPosition;
-            var yPosition: number = r.data["player"].xPosition;
-            var health: number = r.data["player"].xPosition;
+            var name:string = r.data[0].player.name;
+            var xPosition: number = r.data[0].player.yPosition;
+            var yPosition: number = r.data[0].player.xPosition;
+            var health: number = r.data[0].player.xPosition;
 
             var character: ICharacter = new Character(name, xPosition, yPosition, health);
 
             var state = new GameState(npcs, character, layer);
+            
             return state;
         });
     }
