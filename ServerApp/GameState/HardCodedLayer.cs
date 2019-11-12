6eb37e883587a92526aad40855edf71a3b61fcd8
legbeard <au484146@uni.au.dk>
2019-11-12 10:09:06 +0100

    Added stats to character; Started making Hard-Coded map for client team.

---
+++ ServerApp/GameState/HardCodedLayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using ServerApp.GameState;
+
+namespace ServerApp.GameState
+{
+    public class HardCodedLayer: ILayer
+    {
+        public Character[,] NPCs;
+        public ITile[,] Tiles;
+
+        public HardCodedLayer()
+        {
+            int width = 10;
+            int height = 10;
+            NPCs = new Character[width, height];
+            Tiles = new ITile[width,height];
+
+            for (var x = 0; x < width; x++)
+            {
+                for (var y = 0; y < height; y++)
+                {
+                    Tiles[x, y] = new Tile(false);
+                    NPCs[x, y] = null;
+                }
+            }
+
+            for (var x = 0; x < width; x++)
+            {
+                for (var y = 0; y < height; y++)
+                {
+                }
+            }
+        }
+    }
+}
---
+++ ServerApp/GameState/HardCodedMap.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ServerApp.GameState
+{
+    public class HardCodedMap: IMap
+    {
+        public List<ILayer> = new List<HardCodedLayer>
+        public HardCodedMap()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Layers.Add(new Layer(10, 10));
+            }
+        }
+    }
+}
