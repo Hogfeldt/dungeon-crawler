8f473414d92c4fab4f1597c3ade8d88d943b3005
srensommer <srensommer@gmail.com>
2019-11-15 10:56:52 +0100

    Så er der tests up and running

---
+++ ServerApp/Test/UnitTests/GameStateUnitTest/NPC.UnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [SetUp]
+        public void setup()
+        { }
+
+        [Test]
+        public void TestTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
