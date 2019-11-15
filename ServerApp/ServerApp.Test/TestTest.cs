454f3ad68e38bd71a984933f2e98bcde6c12ec68
srensommer <srensommer@gmail.com>
2019-11-15 09:47:45 +0100

    Here be tests

---
+++ ServerApp/ServerApp.Test/TestTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ServerApp.Test
+{   
+    [TestFixture]
+    public class SomethingTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+        [Test]
+        public void ThisIsTestTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+    }
+}
