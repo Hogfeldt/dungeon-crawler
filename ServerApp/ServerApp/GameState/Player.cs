c9eb20aaaab08b7759d837687625abdcffde2e0c
Downz <anderesmhandsome@gmail.com>
2019-11-22 10:16:57 +0100

    Player holds xp and can retrieve more xp

--- ServerApp/ServerApp/GameState/Player.cs
+++ ServerApp/ServerApp/GameState/Player.cs
@@ -5,18 +5,30 @@ namespace ServerApp.GameState
     public class Player: Character
     {
         public int Gold { get; private set; }
+        public int Xp { get; private set;  }
 
         public Player(IPosition position, IStats stats, string name = "Player McName", int gold = 15) : base(position, stats, name)
         {
             Gold = gold;
         }
 
-        public void AddGold(int gold)
+        public void AddExperience(int xp)
+        {
+            if (xp > 0)
+            {
+                Xp += xp;
+            }
+        }
+
+        public bool AddGold(int gold)
         {
             if (gold > 0)
             {
                 Gold += gold;
+                return true;
             }
+
+            return false;
         }
 
         public bool RemoveGold(int gold)
