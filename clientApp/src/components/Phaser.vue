b9d7bca30ffe5306614793ee612675c1283d56f6
eliasrosendahl <jensen.eliasrosendahl@gmail.com>
2019-11-26 16:50:45 +0100

    adds deploy script

--- clientApp/src/components/Phaser.vue
+++ clientApp/src/components/Phaser.vue
@@ -146,7 +146,7 @@ var createLabel = function (scene, text) {
         }, Demo],
         "render.transparent": true
     };    
-    var api: IApi = new Api("http://127.0.0.1:5000");
+    var api: IApi = new Api("http://178.62.43.127:5000");
 
     var handler: ChangeHandler = new ChangeHandler(api);
     var player;
