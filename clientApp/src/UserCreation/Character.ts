582cc69dcc31f0430261a87054e9f6b02d43e6c0
Glenn Laursen <201703930@post.au.dk>
2019-11-30 13:36:58 +0100

    Added new classes for userinfo stuff

---
+++ clientApp/src/UserCreation/Character.ts
@@ -0,0 +1,11 @@
+﻿export class Character {
+    public sprite: string;
+    public color: string;
+    public username: string;
+
+    constructor(sprite: string, color: string, username: string) {
+        this.sprite = sprite;
+        this.color = color;
+        this.username = username;
+    }
+}
