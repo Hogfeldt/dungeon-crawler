9f5b06b9197f71434a7e8f879b6bfec77c41773b
Per Høgfeldt <hogfeldt38@gmail.com>
2019-11-30 21:38:15 +0100

    Use droploot in combat handler

--- ServerApp/ServerApp/TurnExec/CombatHandler.cs
+++ ServerApp/ServerApp/TurnExec/CombatHandler.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ServerApp.GameState;
+﻿using ServerApp.GameState;
 
 namespace ServerApp.TurnExec
 {
@@ -10,38 +6,11 @@ namespace ServerApp.TurnExec
     {
         public void Fight(ICharacter attacker, ICharacter defender)
         {
-            if (attacker.GetType() == typeof(Player) && defender.GetType() == typeof(HostileNPC))
+            defender.TakeDamage(attacker.Stats.Damage);
+            if (!defender.Alive)
             {
-                //character is Player and characterOnTile is HostileNPC, Player punches HostileNPC
-                PlayerFightHostileNPC((Player)attacker, (HostileNPC)defender);
-
+                defender.DropLoot();
             }
-            else if (attacker.GetType() == typeof(HostileNPC) && defender.GetType() == typeof(Player))
-            {
-                //character is HostileNPC and characterOnTile is Player, HostileNPC punches Player.
-                HostileNPCFightPlayer((HostileNPC)attacker);
-            }
-        }
-
-        private void PlayerFightHostileNPC(Player player, HostileNPC hostile)
-        {
-            hostile.TakeDamage(player.Stats.Damage);
-            if (!hostile.Alive)
-            {
-                //The monster died - Award gold and experience to player
-                hostile.DropToCharacter(player);
-
-                //Save the monster position so that we can move the player after cleaning the layer.
-                player.Position = hostile.Position;
-            }
-        }
-
-        private void HostileNPCFightPlayer(HostileNPC hostile)
-        {
-            //Punch the player and ????????????????? if they die.
-            throw new NotImplementedException("HostileNPCFightPlayer isn't implemented yet");
         }
     }
-
-
 }
