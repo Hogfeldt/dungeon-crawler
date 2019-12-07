e6b64c0cc6fecdb40d6cb2513556962ba045b0d1
EliasRosendahl <jensen.eliasrosendahl@gmail.com>
2019-12-07 09:52:42 +0100

    removes toGrid test, as it has been removed

--- ServerApp/Test/UnitTests/TurnExecutionerUnitTest/CharacterFormatterTest.cs
+++ ServerApp/Test/UnitTests/TurnExecutionerUnitTest/CharacterFormatterTest.cs
@@ -37,21 +37,5 @@ namespace Test.UnitTests.TurnExecutionerUnitTest
                 Assert.AreNotEqual(null, character);
             }
         }
-
-        [Test]
-        public void CharacterFormatter_ToGridGivenListContaining3NPC_Returns10x10Grid()
-        {
-            List<ICharacter> characters = new List<ICharacter>();
-
-            characters.Add(new HostileNPC(new Position(1, 2), new Stats(), new StandStillMovementStrategy(), "bad boi 1"));
-            characters.Add(new HostileNPC(new Position(4, 1), new Stats(), new StandStillMovementStrategy(), "bad boi 2"));
-            characters.Add(new HostileNPC(new Position(7, 5), new Stats(), new StandStillMovementStrategy(), "bad boi 3"));
-
-            ICharacter[, ] charactersGrid = _uut.ToGrid(characters);
-
-            Assert.AreEqual("bad boi 1", charactersGrid[1, 2].Name);
-            Assert.AreEqual("bad boi 2", charactersGrid[4, 1].Name);
-            Assert.AreEqual("bad boi 3", charactersGrid[7, 5].Name);
-        }
     }
 }
