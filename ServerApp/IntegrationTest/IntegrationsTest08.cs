6641784bf553f645967841b459e20d7242b2ed65
EliasRosendahl <jensen.eliasrosendahl@gmail.com>
2019-12-09 15:28:59 +0100

    adds IntegrationTest08

---
+++ ServerApp/IntegrationTest/IntegrationsTest08.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ServerApp.GameState;
+using ServerApp.TurnExec;
+
+namespace IntegrationTest
+{
+    class IntegrationsTest08
+    {
+        private ITurnExecutioner _sut;
+        private IMoveExecutioner _moveExecutioner;
+        private IMoveValidator _moveValidator;
+        private IInteractionHandler _interactionHandler;
+        private ITurnScheduler _turnScheduler;
+        private ICharacterFormatter _characterFormatter;
+        private IMovement _movement;
+        private ICombatHandler _combatHandler;
+        private IGameState _gameState;
+
+        [SetUp]
+        public void Setup()
+        {
+            _movement = new Movement();
+            _combatHandler = new CombatHandler();
+            _moveExecutioner = new MoveExecutioner(_combatHandler, _movement);
+
+            _moveValidator = new MoveValidator();
+
+            _interactionHandler = new InteractionHandler();
+
+            _turnScheduler = new TurnScheduler();
+
+            _characterFormatter = new CharacterFormatter();
+
+            _gameState = new GameStateClass(
+                new Map(
+                    new HardCodedLayerGenerator(),
+                    3,
+                    new Player(new Position(), new Stats(2, 2, 1), "john", 10)));
+
+            _sut = new ConcreteTurnExecutioner(
+                _moveValidator,
+                _characterFormatter,
+                _turnScheduler,
+                _moveExecutioner,
+                _interactionHandler);
+        }
+
+        [Test]
+        public void TurnExecutioner_ExecutesTurn_PlayerMovesRight()
+        {
+            // Arrange
+            _gameState.Player.SetNextMove(Direction.Right);
+
+            // Act
+            _sut.ExecuteTurn(_gameState);
+
+            // Assert
+            Assert.AreEqual(1, _gameState.Player.Position.X);
+            Assert.AreEqual(0, _gameState.Player.Position.Y);
+        }
+    }
+}
